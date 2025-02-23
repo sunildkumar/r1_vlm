e323aab69954733d1e326c67ede3838ee3ecd8b7
Sunil Kumar <sdkumar@hmc.edu>
2025-02-23 12:30:55 -0800

    draft of skeleton interface for trainer.

--- src/r1_vlm/trainer/trainer.py
+++ src/r1_vlm/trainer/trainer.py
@@ -1,5 +1,47 @@
+from typing import Callable
+
+from torch.utils.data import Sampler
 from transformers import Trainer
 
+RewardFunc = Callable[[list, list], list[float]]
 
 class VLMR1Trainer(Trainer):
-    pass 
+    '''
+    Custom GRPO trainer for VLM R1. Based off of TRLs GRPOTrainer and my TRL fork, but specifically focused on
+    what is needed for this project. It /will/ not support all possibile training configurations,
+    but instead will be hyperforcused on the needs of this project.
+    '''
+    
+    def __init__(self):
+        raise NotImplementedError()
+    
+    
+    def _set_signature_columns_if_needed(self):
+        raise NotImplementedError()
+    
+    def _get_train_sampler(self) -> Sampler:
+        raise NotImplementedError()
+    
+    def _move_model_to_vllm(self):
+        raise NotImplementedError()
+    
+    
+    def _get_log_probs(self):
+        raise NotImplementedError()
+    
+    def _prepare_inputs(self):
+        raise NotImplementedError()
+
+    
+    def compute_loss(self):
+        raise NotImplementedError()
+    
+    def prediction_step(self):
+        raise NotImplementedError()
+    
+    def log(self):
+        raise NotImplementedError()
+
+    
+    
+    
